1082d2c36f70fcb183cffaf872f77b0d8ed745cc
Steven Sommer <steven.sommer@go.winona.edu>
2023-02-11 14:35:23 -0600

    Add files via upload

---
+++ alyssa.for
@@ -0,0 +1,20 @@
+      program Alyssa
+
+c*********************************************************************
+c
+cc Alyssa is the main program for a message to Alyssa.
+c
+c  Discussion:
+c
+c    HELLO is a simple FORTRAN77 program that says a message for Alyssa.
+c
+c  Author:
+c
+c    Stephan Sommer
+!    This is a comment
+
+      write ( *, '(a)' ) '  Hello, Alyssa!'
+      write ( *, '(a)' ) ' I cant wait to kiss you again!! '
+
+      stop
+      end
